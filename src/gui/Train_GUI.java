8c325373c2eb14716d12535664b5fcf51179c7f1
clrkco <clark_co@dlsu.edu.ph>
2018-07-25 19:52:25 +0800

    first commit

---
+++ src/gui/Train_GUI.java
@@ -0,0 +1,46 @@
+/*
+ * Created by JFormDesigner on Wed Jul 25 19:46:33 CST 2018
+ */
+
+package gui;
+
+import java.awt.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author Clark Co
+ */
+public class Train_GUI extends JFrame {
+    public Train_GUI() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Clark Co
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Clark Co
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
