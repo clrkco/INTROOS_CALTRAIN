7b1fbad64a30f62ce1953142760b5dff64e7bbb4
Patchot29 <maria_patricia_abad@dlsu.edu.ph>
2018-08-08 17:36:44 +0800

    New GUI
    Text field and buttons added

--- src/gui/ControlSemaphores.java
+++ src/gui/ControlSemaphores.java
@@ -16,14 +16,17 @@ public class ControlSemaphores extends JPanel implements ActionListener
     JLabel
             label_train_num,
             label_train_count,
-            label_train_seats;
+            label_train_seats,
+            label_passengers_station;
 
     JTextField
-            textfield_train_seats;
+            textfield_train_seats,
+            textfield_passengers_station;
 
     JButton
+            button_restart_program,
             button_add_train,
-            button_remove_train;
+            button_add_passengers;
 
     Font trainNumFont = new Font("Georgia", Font.PLAIN, 18);
     Font fieldFont = new Font("Segoe UI", Font.PLAIN, 16);
@@ -55,6 +58,19 @@ public class ControlSemaphores extends JPanel implements ActionListener
         textfield_train_seats = new JTextField(10);
         textfield_train_seats.setFont(fieldFont);
 
+        label_passengers_station = new JLabel("Number of Station of the Passengers:");
+        label_passengers_station.setFont(fieldFont);
+
+        textfield_passengers_station = new JTextField(10);
+        textfield_passengers_station.setFont(fieldFont);
+
+        button_restart_program = new JButton("Restart Program");
+        button_restart_program.setFocusPainted(false);
+        button_restart_program.setFont(buttonFont);
+        button_restart_program.setForeground(Color.white);
+        button_restart_program.setBackground(Color.decode("#04bbaa"));
+        button_restart_program.addActionListener(this);
+
         button_add_train = new JButton("Add Train");
         button_add_train.setFocusPainted(false);
         button_add_train.setFont(buttonFont);
@@ -62,22 +78,25 @@ public class ControlSemaphores extends JPanel implements ActionListener
         button_add_train.setBackground(Color.decode("#04bbaa"));
         button_add_train.addActionListener(this);
 
-        button_remove_train = new JButton("Remove Train");
-        button_remove_train.setFocusPainted(false);
-        button_remove_train.setFont(buttonFont);
-        button_remove_train.setForeground(Color.white);
-        button_remove_train.setBackground(Color.decode("#04bbaa"));
-        button_remove_train.addActionListener(this);
+        button_add_passengers = new JButton("Add Passengers");
+        button_add_passengers.setFocusPainted(false);
+        button_add_passengers.setFont(buttonFont);
+        button_add_passengers.setForeground(Color.white);
+        button_add_passengers.setBackground(Color.decode("#04bbaa"));
+        button_add_passengers.addActionListener(this);
     }
 
     public void assembleComponents()
     {
         this.add(label_train_num, "center");
-        this.add(label_train_seats, "center, wrap");
+        this.add(label_train_seats, "center");
+        this.add(label_passengers_station, "center, wrap");
         this.add(label_train_count, "center");
-        this.add(textfield_train_seats, "center, wrap 20");
+        this.add(textfield_train_seats, "center");
+        this.add(textfield_passengers_station, "center, wrap 20");
+        this.add(button_restart_program, "growx, pushx");
         this.add(button_add_train, "growx, pushx");
-        this.add(button_remove_train, "growx, pushx");
+        this.add(button_add_passengers, "growx, pushx");
     }
 
     public boolean checkAddTrain(String seats)
@@ -109,7 +128,12 @@ public class ControlSemaphores extends JPanel implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        if(e.getSource() == button_add_train)
+        if(e.getSource() == button_restart_program)
+        {
+
+        }
+
+        else if(e.getSource() == button_add_train)
         {
             String name = "Train" + (i+1);
             String seats = textfield_train_seats.getText();
@@ -137,32 +161,14 @@ public class ControlSemaphores extends JPanel implements ActionListener
             }
         }
 
-        else if(e.getSource() == button_remove_train)
+        else if(e.getSource() == button_add_passengers)
         {
-            String name = "Train" + (i+1);
-            String seats = textfield_train_seats.getText();
 
-            i++;
 
-            if (checkAddTrain(seats))
-            {
-                if (CalTrain.numOfTrains == 0)
-                {
-                    JOptionPane.showMessageDialog(ControlSemaphores.this, "No Trains Left", "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                }
 
-                else
-                {
-                    int seatInt = Integer.parseInt(textfield_train_seats.getText());
 
-                    CalTrain.dispatchTrain(name.trim(), seatInt, CalTrain.numOfTrains);
 
-                    CalTrain.numOfTrains++;
 
-                    label_train_count.setText(CalTrain.numOfTrains + " out of 16");
-                }
-            }
         }
     }
 }
